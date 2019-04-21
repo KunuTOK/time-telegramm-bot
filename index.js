c39322a0b2952612b78e49517afc35dcd64d4756
KunuTOK <kunutok@mail.ru>
2019-04-21 22:38:02 +0300

    ё

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api');
-const Koa = require('koa');
-const Router = require('koa-router');
-
 const TOKEN = '742875963:AAEtJnKfKL8CjS9WRHJZnZHNo3Rhk2_Bo_8';
-const bot = new TelegramBot(TOKEN, {
-   webhook: {
-     port: 9000
-     ({AutoOpen: false})
-   }
-});
+const bot = new TelegramBot(TOKEN,);
+  webhook: {
+    port: 9000
+    AutoOpen: false
+   };
 
-bot.openWebHook()
-bot.setWebhook(`${('https://lambda-project.netlify.com/.netlify/functions/hello')}/bot${TOKEN}`);
+bot.setWebhook('https://lambda-project.netlify.com/.netlify/functions/hello'/TOKEN);
 bot.on('message', msg => {
   const chatId = msg.chat.id
-  bot.sendMessage(id, 'hello, first.name')
+  bot.sendMessage(id, 'hello', first.name)
 });
 
-bot.onText(/\/time/, (msg, [source, match]) => {
+bot.onText(/\/time/, (msg,  match) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, match);
 });
