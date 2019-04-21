b212073dc8d59e93669669251d074798604ef3d9
KunuTOK <kunutok@mail.ru>
2019-04-21 21:17:46 +0300

    commit

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api');
-//const config = require('config');
 const Koa = require('koa');
 const Router = require('koa-router');
 
-const token = '742875963:AAEtJnKfKL8CjS9WRHJZnZHNo3Rhk2_Bo_8';
-const bot = new TelegramBot(token);
-bot.setWebhook(`${'url'}/bot`);
+const TOKEN = '742875963:AAEtJnKfKL8CjS9WRHJZnZHNo3Rhk2_Bo_8';
+const bot = new TelegramBot(TOKEN, {
+   webhook: {
+     port: 9000
+     ({AutoOpen: false})
+   }
+});
 
-const app = new Koa()
-const router = Router()
-router.post('/bot', ctx => {
-  console.log(ctx)
-  ctx.status = 200
-})
+bot.openWebHook()
+bot.setWebhook(`${('https://lambda-project.netlify.com/.netlify/functions/hello')}/bot${TOKEN}`);
+bot.on('message', msg => {
+  const chatId = msg.chat.id
+  bot.sendMessage(id, 'hello, first.name')
+});
 
-app.use(router.routes())
-
-const port = config.get('port')
-app.listen(port, () => {
-  console.log(`listening on ${port}`)
-})
-
-bot.onText(/\/time/, (msg, match) => {
+bot.onText(/\/time/, (msg, [source, match]) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'https://www.timeserver.ru/compare/san-francisco/moscow');
+  bot.sendMessage(chatId, match);
 });
